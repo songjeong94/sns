09c859e674e1c02cce02a49587b7a071e0da1929
songjeong94 <thdwjd7899@naver.com>
2023-04-13 03:40:39 +0900

    #3 - 포스트 기능 개발을 위한 테스트 작성 및 스켈레톤 코드

---
+++ src/test/java/com/project/sns/controller/PostControllerTest.java
@@ -0,0 +1,60 @@
+package com.project.sns.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.sns.controller.request.PostCreateRequest;
+import com.project.sns.controller.request.UserJoinRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser //로그인이 되어있다는 가정
+    void 포스트작성() throws Exception{
+
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(post("/api/v1/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostCreateRequest(title, body)))
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser //로그인이 안되어있는 가정
+    void 포스트작성시_로그인하지않은경우() throws Exception{
+
+        String title = "title";
+        String body = "body";
+
+        //로그인하지않은경우
+
+        mockMvc.perform(post("/api/v1/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostCreateRequest(title, body)))
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+
+}
---
+++ src/test/java/com/project/sns/service/PostServiceTest.java
@@ -0,0 +1,60 @@
+package com.project.sns.service;
+
+import com.project.sns.exception.ErrorCode;
+import com.project.sns.exception.SnsApplicationException;
+import com.project.sns.model.entity.PostEntity;
+import com.project.sns.model.entity.UserEntity;
+import com.project.sns.repository.PostEntityRepository;
+import com.project.sns.repository.UserEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @MockBean
+    private PostEntityRepository postEntityRepository;
+
+    @Mock
+    private UserEntityRepository userEntityRepository;
+
+    @Test
+    void 포스트작성이_성공한경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        //mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        Assertions.assertDoesNotThrow(() ->postService.create(title, body, userName));
+    }
+
+    @Test
+    void 포스트작성시_요청한유저가_존재하지않는경우() {
+        String title = "title";
+        String body = "body";
+        String userName = "userName";
+
+        //mocking
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () ->postService.create(title, body, userName));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
+    }
+}
